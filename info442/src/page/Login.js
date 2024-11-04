de9fb19b96558e9a66053c0164cb2d138aaea73e
Sarah <shaworth@uw.edu>
2024-11-04 09:31:45 -0800

    updating js

---
+++ info442/src/page/Login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { StyledFirebaseAuth } from 'react-firebaseui';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+
+
+export default function Login(props) {
+  const currentUser = props.currentUser;
+
+  const auth = getAuth();
+
+  const configObj = {
+    signInOptions: [
+      {
+        provider: GoogleAuthProvider.PROVIDER_ID,
+        requireDisplayName: true,
+      }
+    ],
+    signInFlow: 'popup',
+    callbacks: {
+      signInSuccessWithAuthResult: () => false 
+    },
+    credentialHelper: 'none'
+  }
+
+  return (
+    <div className="card bg-light mx-auto">
+      <div className="container card-body">
+        <h1 className="text-center fs-2 fw-bolder">Sign In Here:</h1>
+        <StyledFirebaseAuth uiConfig={configObj} firebaseAuth={auth} />
+      </div>
+    </div>
+  )
+}
---
+++ info442/src/page/Myevent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Myevent.css';
+
+function MyEvent() {
+    return (
+        <div className="app-container">
+            <header>
+                <nav>
+                <h1 className="page-title">PNW Connects</h1>
+                </nav>
+            </header>
+            <div className="container">
+                <aside>
+                    <ul>
+                        <li><Link to="/notification">Notification Center</Link></li>
+                    </ul>
+                </aside>
+                <main>
+                    <section className="welcome">
+                        <h1>Welcome to My Event</h1>
+                    </section>
+                    <section className="upcoming">
+                        <h2>Upcoming Events</h2>
+                        <div className="events">
+                            <div className="event">
+                                <h3>Event Title</h3>
+                            </div>
+                        </div>
+                    </section>
+                </main>
+            </div>
+            <footer>
+                <div className="footer-content">
+                    <p>INFO 442</p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default MyEvent;
