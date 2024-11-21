c7266d3ff8b31e472091d6f78c058f07329f0d99
Sabrina Chan <102693119+sab-chan@users.noreply.github.com>
2024-11-20 16:16:47 -0800

    added favicon

--- info442/src/page/Home.js
+++ info442/src/page/Home.js
@@ -7,62 +7,10 @@ function Home() {
     return (
         <div className="app-container">
             <main className="main">
-                <Event /> 
 
                 <h1 className="text-dark mb-4 text-start fs-2">Featured Events</h1> {/* Added margin-bottom for space */}
-                
-                {/* Card Row */}
-                <div className="row row-cols-1 row-cols-sm-3 row-cols-md-6 g-4">
-                    {/* Card 1 */}
-                    <div className="col">
-                        <div className="card h-100"> 
-                            <div className="card-body d-flex flex-column">
-                                <h2 className="card-title">Event Name</h2>
-                                <div className="mt-auto">
-                                    {/* Button Container */}
-                                    <a href="#" className="btn btn-dark btn-sm w-100 mb-2">Event Details</a>
-                                    <a href="#" className="btn btn-dark btn-sm w-100">RSVP Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Card 2 */}
-                    <div className="col">
-                        <div className="card h-100">
-                            <div className="card-body d-flex flex-column">
-                                <h2 className="card-title">Event Name</h2>
-                                <div className="mt-auto">
-                                    <a href="#" className="btn btn-dark btn-sm w-100 mb-2">Event Details</a>
-                                    <a href="#" className="btn btn-dark btn-sm w-100">RSVP Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Card 3 */}
-                    <div className="col">
-                        <div className="card h-100">
-                            <div className="card-body d-flex flex-column">
-                                <h2 className="card-title">Event Name</h2>
-                                <div className="mt-auto">
-                                    <a href="#" className="btn btn-dark btn-sm w-100 mb-2">Event Details</a>
-                                    <a href="#" className="btn btn-dark btn-sm w-100">RSVP Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Card 4 */}
-                    <div className="col">
-                        <div className="card h-100">
-                            <div className="card-body d-flex flex-column">
-                                <h2 className="card-title">Event Name</h2>
-                                <div className="mt-auto">
-                                    <a href="#" className="btn btn-dark btn-sm w-100 mb-2">Event Details</a>
-                                    <a href="#" className="btn btn-dark btn-sm w-100">RSVP Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Event /> 
+
             </main>
 
             <footer>
