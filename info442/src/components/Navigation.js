1762665f8540f8c7c1b7d3f7bd21bc4659e794b4
Sarah <shaworth@uw.edu>
2024-12-02 10:49:44 -0800

    making navbar visible with smaller media

--- info442/src/components/Navigation.js
+++ info442/src/components/Navigation.js
@@ -14,7 +14,7 @@ function Navigation() {
           <img src="/favicon.ico" alt="favicon" className="navbar-favicon" />
           PNW Connects
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle className="bg-white" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
           <Nav.Link as={Link} to="/" className="nav-link">Home</Nav.Link>
