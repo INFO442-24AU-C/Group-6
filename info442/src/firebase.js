344acf1eebb5e3e4a1a7ec5bda77338656e878e7
DonkeyKong1019 <davisnathan160@gmail.com>
2024-12-06 15:16:56 -0800

    firebase deploy

--- info442/src/firebase.js
+++ info442/src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCE5JhYedJk5QP5LX-Hn2s4ZYuMZyNSpVY",
+  apiKey: "AIzaSyCAzdlJFmpS66zyC5-69qqWDpmr1SQi8FA",
   authDomain: "pnw-connect.firebaseapp.com",
-  projectId: "pnw-connect",
+  projectId: "pnw-connects",
   storageBucket: "pnw-connect.firebasestorage.app",
-  messagingSenderId: "921966180003",
-  appId: "1:921966180003:web:f98cf3555757b9d4353116"
+  messagingSenderId: "77130043910",
+  appId: "1:77130043910:web:c32bb3f2d9433218e42987"
 };
 
 // Initialize Firebase
